52f35fc5642dd1bbf8c539222ebf5e690be5c8f2
Jefry Paul <jefrypaul73@gmail.com>
2025-10-24 14:15:12 +0530

    header created

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './Pages/Home'
+import Header from './Pages/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Header/>
      GIT COLOABRATION
      <Home/>
     </>
